cab91539b15bb8c99910f8c9282c4e0df0bf0cbd
Weili <1571916135@qq.com>
2024-08-16 17:19:12 +0800

    add animation

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;       // The player or object the camera should follow
+    [SerializeField]
+    public Vector3 targetOffset;   // The fixed offset from the target
+    public Vector3 lookOffset;
+    [SerializeField]
+    private float movementSpeed = 5f; // The speed at which the camera follows the target (if smoothing is desired)
+
+    void LateUpdate()
+    {
+        FollowTarget();
+    }
+    
+    void FollowTarget()
+    {
+        // Directly set the camera's position to the target's position plus the offset
+        transform.position = target.position + targetOffset;
+
+        // Optionally, if you want to add a smoothing effect, uncomment the following line
+        // transform.position = Vector3.Lerp(transform.position, target.position + targetOffset, movementSpeed * Time.deltaTime);
+
+        // Make the camera look at the target (optional, if you want the camera to always face the target)
+        Vector3 lookTarget = target.position + lookOffset;
+        transform.LookAt(lookTarget);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float movementSpeed = 5.0f;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HandleMovementInput();
+        HandleRotationInput();
+        HandleShootInput();
+        UpdateAnimation();
+    }
+
+    void HandleMovementInput()
+    {
+        float _horizontal = Input.GetAxis("Horizontal");
+        float _vertical = Input.GetAxis("Vertical");
+
+        Vector3 _movement = new Vector3(_horizontal, 0, _vertical).normalized;
+        transform.Translate(_movement * movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    void HandleRotationInput()
+    {
+        RaycastHit _hit;
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            transform.LookAt(new Vector3(_hit.point.x, transform.position.y, _hit.point.z));
+        }
+    }
+
+    void HandleShootInput ()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            //Shoot
+            animator.SetTrigger("shoot");
+            // PlayerGun.Instance.Shoot();
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            // Reset the shooting trigger to stop the shooting animation
+            animator.ResetTrigger("shoot");
+        }
+    }
+
+    void UpdateAnimation()
+    {
+        float _horizontal = Input.GetAxis("Horizontal");
+        float _vertical = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction relative to the player's forward direction
+        Vector3 movement = new Vector3(_horizontal, 0, _vertical).normalized;
+        Vector3 localMovement = transform.InverseTransformDirection(movement);
+
+        // Update Animator parameters
+        Debug.Log(localMovement.x);
+        animator.SetFloat("MoveX", localMovement.x);
+        animator.SetFloat("MoveZ", localMovement.z);
+    }
+}
